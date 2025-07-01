48ca484b2805b7383f449fc23c737713e93ab007
David Bradshaw <david.bradshaw@ibm.com>
2025-07-01 13:08:21 -0700

    fix: build docs migration

--- typedoc.js
+++ typedoc.js
@@ -1,17 +1,7 @@
 module.exports = {
-  mode: 'file',
   out: 'docs',
   theme: 'default',
-  ignoreCompilerErrors: true,
   excludePrivate: false,
-  excludeNotExported: true,
-  target: 'ES6',
-  moduleResolution: 'node',
-  preserveConstEnums: 'true',
-  stripInternal: 'true',
-  suppressExcessPropertyErrors: 'true',
-  suppressImplicitAnyIndexErrors: 'true',
-  module: 'commonjs',
   name: 'IBM Aspera JavaScript Library',
   hideGenerator: true
 };

a1c15960a9f82b8a99a02ba4869900a6e9371918
David Bradshaw <dabrad26@users.noreply.github.com>
2025-10-20 09:58:27 -0700

    fix: update packages (#136)

---
+++ scripts/version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+MESSAGE=$(git log -1 --pretty=%B | tr '[:upper:]' '[:lower:]')
+
+NEWVERSION=""
+if [[ "$MESSAGE" == *"chore(release)"* ]]; then
+  echo "Not bumping chore release"
+elif [[ "$MESSAGE" == *"breaking change"* ]]; then
+  echo "Bumping major version"
+  NEWVERSION=`npm version major --no-git-tag-version --no-verify`
+elif [[ "$MESSAGE" == *"feat"* ]]; then
+  echo "Bumping minor version"
+  NEWVERSION=`npm version minor --no-git-tag-version --no-verify`
+else
+  echo "Bumping patch version"
+  NEWVERSION=`npm version patch --no-git-tag-version --no-verify`
+fi
+
+if [ "$NEWVERSION" != "" ]; then
+    git add package.json package-lock.json
+    git commit -m "chore(release): $NEWVERSION [ci skip]" --no-verify
+fi
